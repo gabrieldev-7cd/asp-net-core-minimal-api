82e4e444d9da085b2418b0c83a64cd3622250186
gabrieldev7cd <gabriel.alexandre.dev@gmail.com>
2021-11-11 20:36:29 -0300

    Make CreateTodoViewModel, Changes in gitignore

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using MinAPITest.Data;
+using MinAPITest.ViewModels;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppdbContext>();
@@ -7,6 +8,19 @@ var app = builder.Build();
 app.MapGet("v1/todos", (AppdbContext context) => {
     var todos = context.Todos.ToList();
     return Results.Ok(todos);
+});
+
+app.MapPost("v1/todos", (AppdbContext context, CreateTodoViewModel model) => {
+    var todo = model.MapTo();
+    if( !model.IsValid )
+    {
+        return Results.BadRequest(model.Notifications);
+    }
+
+    context.Todos.Add(todo);
+    context.SaveChanges();
+
+    return Results.Created($"/v1/todos/{todo.Id}", todo);
 });
 
 app.Run();
